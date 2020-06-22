e86d4e20d027a161633cbc482947adb0a35d7305
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2020-06-23 02:49:14 +0300

    Some Assertions in Test edited

--- src/test/java/data/RoomTest.java
+++ src/test/java/data/RoomTest.java
@@ -52,4 +52,57 @@ class RoomTest {
         /* would be edited */
         assertTrue(true);
     }
+
+    @Test
+    void addAsignment() {
+    }
+
+    @Test
+    void getRoomCalendar() {
+    }
+
+    @Test
+    void testAddAdmin() {
+        User userObject1 = new User("testUserName1","testMail1");
+        Room roomObject = new Room("testNameOfRoom",userObject1);
+        User userObject2 = new User("testUserName2","testMail2");
+
+        assertTrue(roomObject.addAdmin(userObject2));
+    }
+
+    @Test
+    void testRemoveAdmin() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+
+        assertTrue(roomObject.removeAdmin(userObject));
+    }
+
+    @Test
+    void testGetCodeOfRoom() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+
+        assertNotNull(roomObject.getCodeOfRoom());
+    }
+
+    @Test
+    void testGetNameOfRoom() {
+    }
+
+    @Test
+    void testGetUserAVLTree() {
+    }
+
+    @Test
+    void testGetAdmins() {
+    }
+
+    @Test
+    void testGenerateRoomCode() {
+    }
+
+    @Test
+    void testCompareTo() {
+    }
 }
