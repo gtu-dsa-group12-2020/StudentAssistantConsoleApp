ff0fe4f998d899c022bacbaf3b6b70b4e52eebd0
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2020-06-23 03:12:21 +0300

    Some Assertions in Test edited

--- src/test/java/data/RoomTest.java
+++ src/test/java/data/RoomTest.java
@@ -88,21 +88,43 @@ class RoomTest {
 
     @Test
     void testGetNameOfRoom() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+
+        assertNotNull(roomObject.getNameOfRoom());
     }
 
     @Test
     void testGetUserAVLTree() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+        /* if add user avl tree method, here may be edited */
+        assertNotNull(roomObject.getUserAVLTree());
     }
 
     @Test
     void testGetAdmins() {
+        User userObject1 = new User("testUserName1","testMail1");
+        Room roomObject = new Room("testNameOfRoom",userObject1);
+        User userObject2 = new User("testUserName2","testMail2");
+        roomObject.addAdmin(userObject2);
+
+        assertNotNull(roomObject.getAdmins());
     }
 
     @Test
     void testGenerateRoomCode() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+
+        assertNotNull(roomObject.generateRoomCode());
     }
 
     @Test
     void testCompareTo() {
+        User userObject = new User("testUserName","testMail");
+        Room roomObject = new Room("testNameOfRoom",userObject);
+
+        assertEquals(roomObject.compareTo("testNameOfRoom"),0);
     }
 }
