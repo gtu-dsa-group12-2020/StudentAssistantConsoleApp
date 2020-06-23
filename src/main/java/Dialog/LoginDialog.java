3473cbde615271a89c47b90fca354f92f43c467f
umit <umitaltintas.10@gmail.com>
2020-06-23 18:07:21 +0300

    update for login dialog

--- src/main/java/Dialog/LoginDialog.java
+++ src/main/java/Dialog/LoginDialog.java
@@ -3,13 +3,38 @@ package Dialog;
 import data.Data;
 import data.User;
 
-public class LoginDialog implements Dialog{
+import java.util.Scanner;
 
+public class LoginDialog implements Dialog {
 
 
 	@Override
 	public void startDialog(Data data) {
+		Scanner scanner = new Scanner(System.in);
+		LoginDialog loginDialog = new LoginDialog();
+		int choice = -1;
+		while (choice != 0) {
+			System.out.println("1- Login ");
 
+			System.out.println("0- Exit");
+			choice = scanner.nextInt();
+			switch (choice) {
+				case 1:
+					System.out.print("Username: " );
+					if (loginCall(data,scanner.next()))
+					break;
+				case 0:
+					System.out.println("Going Back!");
+					break;
+				default:
+					System.out.println("Invalid input : " + choice);
+
+			}
+		}
+	}
+
+	private boolean loginCall(Data data, String username){
+		User user = data.getUserById()
 	}
 
 	@Override
