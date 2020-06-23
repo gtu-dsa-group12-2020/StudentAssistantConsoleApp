15d432b6fbb1b95a52fd7de3230f9441b0cf70ee
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2020-06-23 20:46:08 +0300

    Calendar Javadoc (continue)

--- src/main/java/data/Calendar.java
+++ src/main/java/data/Calendar.java
@@ -2,29 +2,63 @@ package data;
 
 import java.util.*;
 
+/**
+ * The type Calendar.
+ */
 public class Calendar {
     private final Map<Date,Assignment> dates;
 
+    /**
+     * Instantiates a new Calendar.
+     */
     public Calendar() {
         this.dates = new HashMap<>();
     }
 
+    /**
+     * Add assignment to date assignment.
+     *
+     * @param date       the date
+     * @param assignment the assignment
+     * @return the assignment
+     */
     public Assignment addAssignmentToDate(Date date,Assignment assignment) {
         return dates.putIfAbsent(date,assignment);
     }
 
+    /**
+     * Merge calendars.
+     *
+     * @param roomCalendar the room calendar
+     */
     public void mergeCalendars(Calendar roomCalendar) {
         dates.putAll(roomCalendar.dates);
     }
 
+    /**
+     * Remove assignment assignment.
+     *
+     * @param date the date
+     * @return the assignment
+     */
     public Assignment removeAssignment(Date date) {
         return dates.remove(date);
     }
 
+    /**
+     * Gets dates.
+     *
+     * @return the dates
+     */
     public Map<Date, Assignment> getDates() {
         return dates;
     }
 
+    /**
+     * Return calendar string.
+     *
+     * @return the string
+     */
     public String returnCalendar() {
         Map<Date,Assignment> treeMap = new TreeMap<>(this.dates);
         StringBuilder sb = new StringBuilder();
