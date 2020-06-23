01cf8137f2defe542b99fa4c7393217ee38c8afc
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2020-06-23 20:21:07 +0300

    Assignment, Notification, Data Javadoc (continue)

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -3,15 +3,33 @@ package data;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The type Data.
+ */
 public class Data {
+    /**
+     * The User map.
+     */
     Map<String, User> userMap;
+    /**
+     * The Room map.
+     */
     Map<String, Room> roomMap;
 
+    /**
+     * Instantiates a new Data.
+     */
     public Data() {
         userMap = new HashMap<>();
         roomMap = new HashMap<>();
     }
 
+    /**
+     * Add user boolean.
+     *
+     * @param user the user
+     * @return the boolean
+     */
     public boolean addUser(User user) {
         if (user == null)
             return false;
@@ -23,6 +41,12 @@ public class Data {
         }
     }
 
+    /**
+     * Add room boolean.
+     *
+     * @param room the room
+     * @return the boolean
+     */
     public boolean addRoom(Room room) {
         if (roomMap == null)
             return false;
@@ -34,18 +58,42 @@ public class Data {
         }
     }
 
+    /**
+     * Gets room by id.
+     *
+     * @param key the key
+     * @return the room by id
+     */
     public Room getRoomById(String key) {
         return roomMap.getOrDefault(key, null);
     }
 
+    /**
+     * Gets user by id.
+     *
+     * @param key the key
+     * @return the user by id
+     */
     public User getUserById(String key) {
         return userMap.getOrDefault(key, null);
     }
 
+    /**
+     * Remove user by id user.
+     *
+     * @param key the key
+     * @return the user
+     */
     public User removeUserById(String key) {
         return userMap.remove(key);
     }
 
+    /**
+     * Remove room by id room.
+     *
+     * @param key the key
+     * @return the room
+     */
     public Room removeRoomById(String key) {
         return roomMap.remove(key);
     }
